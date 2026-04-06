485499bedd48fcbb7cde88e2d86bbc7bc8ffd52e
Hoang Duc <duonghoangduc1325@gmail.com>
2026-04-06 12:23:24 +0700

    connect mongodb env variables

--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
+/* eslint-disable no-console */
 import express from 'express'
 import exitHook from 'async-exit-hook'
 
-import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
+
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
 
 const START_SERVER = () => {
 
   const app = express()
 
-  const hostname = 'localhost'
-  const port = 8017
-
   app.get('/', async (req, res) => {
-    console.log(await GET_DB().listCollections().toArray())
     res.end('<h1>Hello World!</h1><hr>')
   })
 
-  app.listen(port, hostname, () => {
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
-    console.log(`3. Back-end Server is running success at http://${ hostname }:${ port }/`)
+    console.log(`3. I am ${env.AUTHOR} Back-end Server is running success at http://${ env.APP_HOST }:${ env.APP_PORT }/`)
   })
 
   exitHook(async () => {
