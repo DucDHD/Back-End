81c54ecaa463ab46e19f0a73b8956d312a03baf3
Hoang Duc <duonghoangduc1325@gmail.com>
2026-04-07 19:20:14 +0700

    error handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,15 +1,11 @@
+/* eslint-disable no-console */
 import { StatusCodes } from 'http-status-codes'
 
 const createNew = (req, res, next) => {
   try {
     console.log(req.body)
     res.status(StatusCodes.CREATED).json({ message: 'POST from Controller : APIs create new board' })
-  } catch (error) {
-    console.log(error)
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+  } catch (error) { next(error) }
 }
 
 export const boardController = {
