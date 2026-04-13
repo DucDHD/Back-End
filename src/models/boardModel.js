3341e5da644ec8f4667a05e303a903c2a7143db2
Hoang Duc <duonghoangduc1325@gmail.com>
2026-04-13 18:18:30 +0700

    dnd Update Column DB

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -3,7 +3,7 @@ import { ObjectId } from 'mongodb'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 import { GET_DB } from '~/config/mongodb'
 import { BOARD_TYPES } from '~/utils/constants'
-import { columnModel } from '~/models/columnModel.js'
+import { columnModel } from '~/models/ColumnModel'
 import { cardModel } from '~/models/cardModel'
 
 // Define collection (Name & Schema)
@@ -21,6 +21,8 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   updatedAt: Joi.date().timestamp('javascript').default(null),
   _destroy: Joi.boolean().default(false)
 })
+
+const INVALID_UPDATE_FIELDS = ['_id', 'createdAt']
 
 const validateBeforeCreate = async (data) => {
   return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
@@ -72,7 +74,24 @@ const pushColumnOrderIds = async (column) => {
       { $push: { columnOrderIds: new ObjectId(String(column._id)) } },
       { returnDocument : 'after' }
     )
-    return result.value
+    return result
+  } catch (error) { throw new Error(error) }
+}
+
+const update = async (boardId, updateData ) => {
+  try {
+    Object.keys(updateData).forEach( fieldName => {
+      if (INVALID_UPDATE_FIELDS.includes(fieldName)) {
+        delete updateData[fieldName]
+      }
+    } )
+
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOneAndUpdate(
+      { _id: new ObjectId(String(boardId)) },
+      { $set: updateData },
+      { returnDocument : 'after' }
+    )
+    return result
   } catch (error) { throw new Error(error) }
 }
 
@@ -82,5 +101,6 @@ export const boardModel = {
   createNew,
   findOneById,
   getDetails,
-  pushColumnOrderIds
+  pushColumnOrderIds,
+  update
 }
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -36,7 +36,19 @@ const getDetails = async (boardId) => {
   } catch (error) { throw error }
 }
 
+const update = async (boardId, reqBody) => {
+  try {
+    const updateData = {
+      ...reqBody,
+      updatedAt: Date.now()
+    }
+    const UudatedBoard = await boardModel.update(boardId, updateData)
+    return UudatedBoard
+  } catch (error) { throw error }
+}
+
 export const boardService = {
   createNew,
-  getDetails
+  getDetails,
+  update
 }
